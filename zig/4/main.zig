1bf3c7a558b7049708f5dffd4ac17303ab11fe20
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-01-07 09:57:23 -0600

    slightly more manual string replacement

--- zig/4/main.zig
+++ zig/4/main.zig
@@ -1,12 +1,27 @@
 const std = @import("std");
-const fs = std.fs;
+const mem = std.mem;
+const heap = std.heap;
 
-var stdout_buf: [1024]u8 = undefined;
-var stdout_wr = fs.File.stdout().writer(&stdout_buf);
+var stdout_buf:[1024]u8 = undefined;
+var stdout_wr = std.fs.File.stdout().writer(&stdout_buf);
 const stdout = &stdout_wr.interface;
 
 pub fn main() !void {
-    try stdout.print("foo\n", .{});
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const fi = @embedFile("test.html");
+    const t:[]const u8 = "<!-- split here -->";
+    const new:[]const u8 = "/bT5M[>.n|'N@I\"_UYBHVA&oXryFkV&jq-b3#'5Gbv3t<d\"y-i.s+!BGhU[LKT|->DhlJ5@)WMZ4k'w.>o]b)<?}9hjJ!8[e\"-";
+    
+    const newSi = std.mem.replacementSize(u8, fi, t, new);
+
+    const newStr = try alloc.alloc(u8, newSi);
+    defer alloc.free(newStr);
+
+    _ = std.mem.replace(u8, fi, t, new, newStr);
+
+    try stdout.print("{s}", .{newStr});
     try stdout.flush();
 }
-
